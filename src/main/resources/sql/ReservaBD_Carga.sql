5a57ba235174b35a5b27b90794c255febf38cfa9
yon026 <yonacuna@gmail.com>
2025-06-24 19:49:27 -0300

    Actualizacion de scripts de creacion y carga de datos de prueba para ReservaBD

---
+++ src/main/resources/sql/ReservaBD_Carga.sql
@@ -0,0 +1,31 @@
+--use ReservaBD;
+
+-- Datos para Canchas
+INSERT INTO Canchas (nombreCancha) VALUES ('futbol');
+INSERT INTO Canchas (nombreCancha) VALUES ('padel');
+INSERT INTO Canchas (nombreCancha) VALUES ('voley');
+
+-- Datos para Horarios
+INSERT INTO Horarios (hora) VALUES ('16:00:00');
+INSERT INTO Horarios (hora) VALUES ('17:00:00');
+INSERT INTO Horarios (hora) VALUES ('18:00:00');
+INSERT INTO Horarios (hora) VALUES ('19:00:00');
+INSERT INTO Horarios (hora) VALUES ('20:00:00');
+INSERT INTO Horarios (hora) VALUES ('21:00:00');
+INSERT INTO Horarios (hora) VALUES ('22:00:00');
+INSERT INTO Horarios (hora) VALUES ('23:00:00');
+INSERT INTO Horarios (hora) VALUES ('00:00:00');
+
+--Datos de prueba para Reservas 19/06
+INSERT INTO Reservas (nombres, apellidos, fecha, fkcancha, fkhorario) VALUES
+('Juan', 'Perez', '2025-06-17', 1, 1),  --Masculino, 28, 17/06, Futbol, 16:00
+('Maria', 'Gomez','2025-06-17', 2, 2), --Femenino, 35, 17/06, Padel, 17:00
+('Carlos', 'Lopez','2025-06-17', 3, 3), --Masculino, 22, 17/06, Voley, 18:00
+('Ana', 'Rodriguez','2025-06-18', 1, 4), --Femenino, 41, 18/06, Futbol, 19:00
+('Pedro', 'Martinez','2025-06-18', 2, 5),--Masculino, 30, 18/06, Padel, 20:00
+('Laura', 'Diaz', '2025-06-18', 3, 6),  --Femenino, 25, 18/06, Voley, 21:00
+('Sofia', 'Hernandez', '2025-06-19', 1, 7),--Femenino, 19, 19/06, Futbol, 22:00
+('Diego', 'Sanchez', '2025-06-19', 2, 8),--Masculino, 33, 19/06, Padel, 23:00
+('Valeria', 'Torres','2025-06-19', 3, 9),--Femenino, 29, 19/06, Voley, 00:00
+('Gabriel', 'Ruiz', '2025-06-20', 1, 1); --Masculino, 45, 19/06, Futbol, 16:00
+
--- src/main/resources/sql/ReservaBD_Creacion.sql
+++ src/main/resources/sql/ReservaBD_Creacion.sql
@@ -1,15 +1,10 @@
--- CREATE DATABASE ReservasDB;
-
-CREATE TABLE Sexos (
-    id INT PRIMARY KEY IDENTITY(1,1),
-    descripcion NVARCHAR(50) NOT NULL UNIQUE
-);
+-- CREATE DATABASE ReservaBD;
+use ReservaBD;
 
 CREATE TABLE Canchas (
     id INT PRIMARY KEY IDENTITY(1,1),
     nombreCancha NVARCHAR(50) NOT NULL UNIQUE
 );
-
 
 CREATE TABLE Horarios (
     id INT PRIMARY KEY IDENTITY(1,1),
@@ -21,8 +16,6 @@ CREATE TABLE Reservas (
     id INT PRIMARY KEY IDENTITY(1,1),
     nombres NVARCHAR(100) NOT NULL,
     apellidos NVARCHAR(100) NOT NULL,
-    fksexo INT NOT NULL,
-    edad INT NOT NULL,
     fecha DATE NOT NULL,
     fkcancha INT NOT NULL,
     fkhorario INT NOT NULL
@@ -30,14 +23,10 @@ CREATE TABLE Reservas (
 
 --Incorporacion de llaves foraneas
 ALTER TABLE Reservas
-ADD CONSTRAINT FK_Reservas_Sexos FOREIGN KEY (fksexo) REFERENCES Sexos(id);
-
-ALTER TABLE Reservas
 ADD CONSTRAINT FK_Reservas_Canchas FOREIGN KEY (fkcancha) REFERENCES Canchas(id);
 
 ALTER TABLE Reservas
 ADD CONSTRAINT FK_Reservas_Horarios FOREIGN KEY (fkhorario) REFERENCES Horarios(id);
-
 
 --Incorporando Restriccion UNIQUE (para evitar sobre-reservas en una misma /fecha/cancha/hora)
 ALTER TABLE Reservas
